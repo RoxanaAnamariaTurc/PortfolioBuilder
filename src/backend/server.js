9e5f4adbd750cc3c863ef4f7e68f16a84442cf93
RoxanaAnamariaTurc <roxi.anamaria25@gmail.com>
2024-06-19 13:04:01 +0100

    Updated cors

--- src/backend/server.js
+++ src/backend/server.js
@@ -11,28 +11,30 @@ import { v4 as uuidv4 } from "uuid";
 dotenv.config();
 app.use(express.json());
 
-const allowedOrigins =
-  process.env.NODE_ENV === "production"
-    ? ["https://nimble-fairy-0b2928.netlify.app"]
-    : ["http://localhost:3002"];
+// const allowedOrigins =
+//   process.env.NODE_ENV === "production"
+//     ? ["https://nimble-fairy-0b2928.netlify.app"]
+//     : ["http://localhost:3002"];
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     credentials: true,
+//   })
+// );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", allowedOrigins.join(", "));
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", allowedOrigins.join(", "));
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+app.use(cors());
 
 const UserSchema = new mongoose.Schema({
   fullName: String,
